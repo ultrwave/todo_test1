bcbd90ae29450a24bdc19d3beae543d5a3833849
Dmitry DM <ultrwave@gmail.com>
2020-11-24 19:14:11 +0300

    basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <div className='todoList'>
+          <h3>Things to do:</h3>
+          <div>
+            <input className='input'/>
+            <button className='addTaskButton'>+</button>
+          </div>
+          <ul>
+            <li><input type="checkbox" checked={true}/> <span>Study</span></li>
+            <li><input type="checkbox" checked={true}/> <span>Eat</span></li>
+            <li><input type="checkbox" checked={false}/> <span>Sleep</span></li>
+          </ul>
+          <div className='fButtons'>
+            <button className='filterButton'>All</button>
+            <button className='filterButton'>Active</button>
+            <button className='filterButton'>Completed</button>
+          </div>
+        </div>
+      </div>
   );
 }
 
